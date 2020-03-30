0a0141b75a764165522f32e6e5254f163b464d78
at-tannguyen2 <tan.nguyen2@asiantech.vn>
2020-03-30 08:54:34 +0700

    Angular excercise day 5

--- favorite-app/src/app/togge-favourite.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToggeFavouriteDirective } from './togge-favourite.directive';
-
-describe('ToggeFavouriteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ToggeFavouriteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
